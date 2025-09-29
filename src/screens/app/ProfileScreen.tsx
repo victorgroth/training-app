e669571732216a028b00360fe275de4fd9734945
VictorGroth <vgroth@hotmail.se>
2025-09-29 14:23:37 +0200

    Lade till logga ut-funktion i ProfileScreen som rensar AsyncStorage och navigerar till AuthStack (#11)

--- src/screens/app/ProfileScreen.tsx
+++ src/screens/app/ProfileScreen.tsx
@@ -1,15 +1,19 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, { useContext } from 'react';
+import { View, Text, Button, StyleSheet } from 'react-native';
+import { AuthContext } from '../../context/AuthContext';
 
 export default function ProfileScreen() {
+    const { logout } = useContext(AuthContext);
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Profil</Text>
+      <Button title="Logga ut" onPress={logout} />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: { flex: 1, justifyContent: 'center', alignItems: 'center' },
-  title: { fontSize: 20, fontWeight: 'bold' },
+  title: { fontSize: 20, fontWeight: 'bold', marginBottom: 20  },
 });
